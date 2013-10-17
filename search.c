c98cb64b1ff2735931a5d1b88a51a6207eb3287a
Amos Waterland <apw@seas.harvard.edu>
2013-10-16 20:58:22 -0400

    Clarify cache search logic.

--- search.c
+++ search.c
@@ -2,16 +2,16 @@
 
 /* Walk the linked list of cached tunnel entries.
  * Return the first tunnel whose begin state vector is 
- * equivalent to x up to symmetries.
+ * equivalent to state vector x up to symmetries.
  * Return 0 if no match found.
  */
 tunnel_t *search(uint8_t const *x, tunnel_t *head)
 {
-    tunnel_t *t = 0;
+    tunnel_t *t;
 
     for (t = head; t; t = t->next)
         if (congruent(x, t))
             return t;
 
-    return t;
+    return 0;
 }
