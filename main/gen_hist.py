65f5ff680607df9835e744ee3302ecf1a1a6a685
Yaniv Yacoby <yanivyacoby@college.harvard.edu>
2013-11-02 15:01:08 -0400

    graphs ignore data > 99.7th percentile so that its easier to read the graphs

--- main/gen_hist.py
+++ main/gen_hist.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pylab
 
-MAX_PERCENT_OUTLIERS = 0.005
-OUTLIER_LATENCY = 1400000
+PERCENTILE = 99.7
+BINS = 100
 
 # read in data from file
 fname = sys.argv[1]
@@ -23,9 +23,6 @@ for line in lines:
     split_line = line.split(" ")
     hm = split_line[0]
     num = int(split_line[1])
-    if (num > OUTLIER_LATENCY):
-        num_outliers = num_outliers + 1
-        continue
     if (hm == "M"):
         m.append(num)
     elif (hm == "H"):
@@ -33,18 +30,21 @@ for line in lines:
     else:
         assert(0)
 
-print "Number of Outliers = " + str(num_outliers) + " of " + str(len(m) + len(h))
-assert(num_outliers < (len(m) + len(h)) * MAX_PERCENT_OUTLIERS)
+
+h_max = np.percentile(h, PERCENTILE)
+m_max = np.percentile(h, PERCENTILE)
 
 def create_hist(dat, col, lab):
-    return pylab.hist(dat, bins=100, histtype='bar',
+    return pylab.hist(dat, bins=BINS, histtype='bar',
                       weights=np.ones_like(dat)/float(len(dat)),
                       color=[col],
                       label=[lab],
                       alpha=0.5)
 
-n, bins, patches = create_hist(h, 'blue', 'hits')
-n, bins, patches = create_hist(m, 'red', 'misses')
+n, bins, patches = create_hist([i for i in h if i <= h_max],
+                               'blue', 'hits')
+n, bins, patches = create_hist([i for i in m if i <= m_max],
+                               'red', 'misses')
 
 pylab.legend()
 pylab.title('Hit & Miss Distribution for \n' + os.path.basename(fname))
