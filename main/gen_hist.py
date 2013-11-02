733dd6406504918aa87cb38ae9ee4d387fe8e6e8
Yaniv Yacoby <yanivyacoby@college.harvard.edu>
2013-11-02 00:33:47 -0400

    modified gen_hist to normalize bars

--- main/gen_hist.py
+++ main/gen_hist.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pylab
 
+MAX_PERCENT_OUTLIERS = 0.005
+OUTLIER_LATENCY = 1400000
+
 # read in data from file
 fname = sys.argv[1]
 with open(fname) as f:
@@ -13,11 +16,16 @@ with open(fname) as f:
 h = []
 m = []
 
+num_outliers = 0
+
 # read in each line, separating hits and misses
 for line in lines:
     split_line = line.split(" ")
     hm = split_line[0]
     num = int(split_line[1])
+    if (num > OUTLIER_LATENCY):
+        num_outliers = num_outliers + 1
+        continue
     if (hm == "M"):
         m.append(num)
     elif (hm == "H"):
@@ -25,15 +33,23 @@ for line in lines:
     else:
         assert(0)
 
-# append the two lists into a meta-list
-final_list = []
-final_list.append(m)
-final_list.append(h)
+print "Number of Outliers = " + str(num_outliers) + " of " + str(len(m) + len(h))
+assert(num_outliers < (len(m) + len(h)) * MAX_PERCENT_OUTLIERS)
 
-# plot the meta-list grouped by hit/miss
-n, bins, patches = pylab.hist(final_list, 10, normed=1, histtype='bar',
-                              color=['red', 'blue'],
-                              label=['miss', 'hit'])
+def create_hist(dat, col, lab):
+    return pylab.hist(dat, bins=100, histtype='bar',
+                      weights=np.ones_like(dat)/float(len(dat)),
+                      color=[col],
+                      label=[lab],
+                      alpha=0.5)
+
+n, bins, patches = create_hist(h, 'blue', 'hits')
+n, bins, patches = create_hist(m, 'red', 'misses')
 
 pylab.legend()
-pylab.show()
+pylab.title('Hit & Miss Distribution for \n' + os.path.basename(fname))
+
+# store file in the graphs directory
+pngname, fileext = os.path.splitext(os.path.basename(fname))
+pylab.savefig('graphs/' + pngname + '.png')
+#pylab.show()
